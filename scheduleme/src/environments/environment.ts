215a8563a776f141db9fd0504b76ff80f63d4120
theegovernor <neilamin@hotmail.com>
2018-04-11 21:03:16 -0400

    set up project and added angularfire and auth for firbase

---
+++ scheduleme/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCYsOT7cUC55iQsiVNgRbXVtSMLCicYBKo",
+    authDomain: "scheduleme-382fb.firebaseapp.com",
+    databaseURL: "https://scheduleme-382fb.firebaseio.com",
+    projectId: "scheduleme-382fb",
+    storageBucket: "scheduleme-382fb.appspot.com",
+    messagingSenderId: "328231410055"
+  }
+};
