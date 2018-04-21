506b8ddbb201a61602a2ad55552938fd87c3a320
qin72 <qin72@purdue.edu>
2018-04-21 04:20:45 -0400

    migrate main layout to home component

--- scheduleme/src/app/calendar/calendar.component.ts
+++ scheduleme/src/app/calendar/calendar.component.ts
@@ -2,13 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService} from '../services/auth.service';
 import { Router } from '@angular/router';
 import { CalendarEvent } from 'angular-calendar';
-import { MatButtonModule } from '@angular/material';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { AllEventComponent } from '../all-event/all-event.component';
-import { CatagoryEventComponent } from '../catagory-event/catagory-event.component';
-import { TodayEventComponent } from '../today-event/today-event.component';
+
 
 
 
--- scheduleme/src/app/home/home.component.ts
+++ scheduleme/src/app/home/home.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService} from '../services/auth.service';
 import { Router } from '@angular/router';
 import { CalendarEvent } from 'angular-calendar';
+import { MatButtonModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { AllEventComponent } from '../all-event/all-event.component';
+import { CatagoryEventComponent } from '../catagory-event/catagory-event.component';
+import { TodayEventComponent } from '../today-event/today-event.component';
+import { CalendarComponent } from '../calendar/calendar.component'
 
 @Component({
   selector: 'app-home',
@@ -22,10 +30,6 @@ export class HomeComponent implements OnInit {
     this.router.navigateByUrl('');
 
   }
-  
-  calendarView()
-  {
-    this.router.navigateByUrl('calendar');
-  }
+
 
 }
