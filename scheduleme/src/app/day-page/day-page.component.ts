759f558ffde29e81d8d910c8e5f45b2063b38056
Jackson Sorrells <sorrelj@purdue.edu>
2018-04-23 15:01:13 -0400

    Solved router issue

--- scheduleme/src/app/day-page/day-page.component.ts
+++ scheduleme/src/app/day-page/day-page.component.ts
@@ -1,6 +1,8 @@
 
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router"
+import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
+import { Event } from '../objects/event';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-day-page',
@@ -14,6 +16,8 @@ export class DayPageComponent implements OnInit {
 
   dateClicked: Date;
 
+  newEvent: Event;
+
   categories=[
     'School',
     'Extracurriculars',
@@ -22,13 +26,14 @@ export class DayPageComponent implements OnInit {
   ];
 
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
     this.route.queryParams.subscribe(params =>{
       this.dateClicked = params["Date"];
     })
    }
 
   addEvent(){
+    alert("Event Added");
 
   }
 
