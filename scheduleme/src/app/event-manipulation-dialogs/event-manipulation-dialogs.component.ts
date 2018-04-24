a0e81c5e7e87966c50a4000376f1a43fe321d4ec
qin72 <38231104+qin72@users.noreply.github.com>
2018-04-23 23:57:37 -0400

    solve confliction

--- scheduleme/src/app/event-manipulation-dialogs/event-manipulation-dialogs.component.ts
+++ scheduleme/src/app/event-manipulation-dialogs/event-manipulation-dialogs.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Event } from '../objects/event';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {MatDialog} from '@angular/material';
+import {MatDialogModule} from '@angular/material/dialog';
+
 import {AuthService} from '../services/auth.service';
 import {  AnotherDialog } from './another-dialog.component';
 import {  OneDialog} from './one-dialog.component';
@@ -20,34 +22,25 @@ export class EventManipulationDialogsComponent implements OnInit {
   ngOnInit() {
   }
 
-  /*
-  add_dialog() {
-    var res;
-    let dialogRef = this.dialog.open(OneDialog, {
-      width: '350px',
-      data: { event: new Event() }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      res = result;
-    });
-    return res;
-  }
-*/
 
   update_dialog(e) {
     var res;
+    if(e != null){
+      res = new Event();
+      res.eDate = e.eDate;
+      res.eventDesc = e.eventDesc;
+      res.timestamp = e.timestamp;
+      res.category = e.category;
+      res.eventName = e.eventName;
+    } else {
+      res = new Event();
+    }
     let dialogRef = this.dialog.open(OneDialog, {
       width: '350px',
-      data: { event: e }
+      data: { event: res }
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      res = result;
-    });
-    return res;
+    return dialogRef.afterClosed();
   }
 
 
@@ -58,11 +51,6 @@ export class EventManipulationDialogsComponent implements OnInit {
       data: {a: "nah"}
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      res = result;
-    });
-
-    return res;
+    return dialogRef.afterClosed();
   }
 }
